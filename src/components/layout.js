1ea3c6b21bfb1b7f07daced097321f648830b209
Sam Clift <sam.clift@adarga.ai>
2018-11-23 16:26:36 +0000

    update the content

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,8 +22,8 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Sams Blog' },
+            { name: 'keywords', content: 'Blog, JS, CSS, Programming' },
           ]}
         >
           <html lang="en" />
