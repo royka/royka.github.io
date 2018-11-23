d2f9a50a98714f64e518e22a31717f6e29ba26da
Sam Clift <sam.clift@adarga.ai>
2018-11-23 15:49:06 +0000

    replace with gatsby

--- _posts/2016-03-28-welcome-to-jekyll.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title:  "Welcome to my Jekyll Blog!"
-date:   2016-03-28 16:24:50 +0100
-categories: jekyll update
----
-I have been doing a lot of interesting things recently and I would like to share them with anyone who is interested!
-Expect to see code, hackathons, running and much more.
---
+++ src/pages/shake.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+import './shake.css'
+
+const ShakeCSSPage = () => (
+  <Layout>
+    <Link to="/">Go back to the homepage</Link>
+    <h1>An Example of a CSS animation on an image</h1>
+    <div classNAme="face">
+        <Image />
+    </div>
+  </Layout>
+)
+
+export default ShakeCSSPage
