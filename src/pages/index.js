c53dd6d2d31dd28f30de58d98da19e8f118f9916
Sam Clift <sam.clift@adarga.ai>
2018-11-23 16:19:42 +0000

    flip cool code

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,14 @@ import './index.css'
 
 const IndexPage = () => (
     <Layout>
+        <div className='coolCode'>
+            <h3>Cool Code</h3>
+            <Link to="/shake/">Shake Animation</Link>
+        </div>
         <div className='aboutMe'>
             <h3>About Me</h3>
             <Link to="/cv/">CV</Link>
             <Link to="/open-source/">Open Source</Link>
-        </div>
-        <div className='coolCode'>
-            <h3>Cool Code</h3>
-            <Link to="/shake/">Shake Animation</Link>
         </div>
     </Layout>
 )
